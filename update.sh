3dd7f9be10fac6988f2a4aefb3e5b65efeb5036a
Kenji Saito <ken-yo@mbr.nifty.com>
2026-05-10 08:46:50 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -21,11 +21,10 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-npx -y pnpm@latest self-update && pnpm install -r && rm -rf pnpm-lock.yaml && pnpm up -r && pnpm audit --fix && pnpm up -r && pnpm lint-fix && pnpm build
-result=$?
-if [ $result -ne 0 ]; then
+
+if ! (npx -y pnpm@latest self-update && pnpm install -r && rm -rf pnpm-lock.yaml && pnpm up -r && pnpm audit --fix override && pnpm up -r && pnpm lint-fix && pnpm build); then
   cd "${CUR}" || exit
-  exit $result
+  exit 1
 fi
 
 cd "${CURRENT}" || exit
